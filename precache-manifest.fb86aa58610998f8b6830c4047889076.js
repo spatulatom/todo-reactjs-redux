40d9e54a165d77e452923311d6ddfcac48ad076d
spatulatom <spatulatom@gmail.com>
2022-04-23 18:02:27 +0100

    Updates

---
+++ precache-manifest.fb86aa58610998f8b6830c4047889076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a422df3b74ea1afa1a1fd541f8768bb8",
+    "url": "/todo-reactjs-redux/index.html"
+  },
+  {
+    "revision": "67e1e60fc7ae32c72848",
+    "url": "/todo-reactjs-redux/static/css/main.4d6409e8.chunk.css"
+  },
+  {
+    "revision": "c5e64ae7e029df113000",
+    "url": "/todo-reactjs-redux/static/js/2.b699185a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-reactjs-redux/static/js/2.b699185a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e1e60fc7ae32c72848",
+    "url": "/todo-reactjs-redux/static/js/main.e06f7739.chunk.js"
+  },
+  {
+    "revision": "b29301d371652f8b5e99",
+    "url": "/todo-reactjs-redux/static/js/runtime-main.91d1625b.js"
+  }
+]);
